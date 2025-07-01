602fe6991b868f226c77ddd3f2db5b64a6b6cdaa
Marina Nelson <marinarasauced@outlook.com>
2025-06-30 20:25:43 -0400

    convert simulation launch file to python

---
+++ launch/simulation.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="rosmav",
+            executable="ros_bluerov2_interface",
+            name="bluerov2_interface",
+            namespace="rov1",
+            parameters=[{
+                "udp_params": "udpin:0.0.0.0:9002"
+            }]
+        ),
+        Node(
+            package="rosmav",
+            executable="ros_bluerov2_interface",
+            name="bluerov2_interface",
+            namespace="rov2",
+            parameters=[{
+                "udp_params": "udpin:0.0.0.0:9012"
+            }]
+        )
+    ])
